01f1f0a486e75c7b8f25d3c8399b3de7965f92bc
garettarrowood <garettarrowood@gmail.com>
2015-07-25 11:47:16 -0400

    Done. Joy.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -7,9 +7,36 @@ class Jukebox
 	end
 
 	def call
+		puts "Welcome! Input your command now:"
+		input = gets.chomp
+		until input == "exit"
+			case input
+			when /help/
+				help
+				input = gets.chomp.downcase
+			when /list/
+				list
+				input = gets.chomp.downcase
+			when /play/
+				input_array = input.split
+				if input_array.length == 1
+				  play
+				else
+					input_array.shift
+					input = input_array.join(" ")
+					play(input)
+				end
+				input = gets.chomp.downcase
+			else
+				puts "That command is not recognized. Type 'help' to review your options."
+				input = gets.chomp.downcase
+			end
+		end
+		puts "Goodbye!"
 	end
 
 	def help
+		puts "Here are your possible commands:"
 		puts "help"
 		puts "list"
 		puts "play"
@@ -29,16 +56,11 @@ class Jukebox
 		  puts "/Now Playing: #{song[0]}/"
 		else
 			@songs.each_with_index do |song2, index|
-				if index == song[0].to_i
-					binding.pry
-					"/Now Playing: #{song2}/"
+				if index + 1 == song[0].to_i
+					puts "/Now Playing: #{song2}/"
 				end
 			end
 		end
 	end
 
 end
-
-pop = Jukebox.new(["mom","dad"])
-pop.play
-pop.play("daddo")
